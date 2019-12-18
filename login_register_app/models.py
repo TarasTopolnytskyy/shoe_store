b70dc4773525fa0744a7d8b2422634f7aecb5921
bstant12 <stantonb12@gmail.com>
2019-12-18 10:47:24 -0600

    Fixed things and added a few things

--- login_register_app/models.py
+++ login_register_app/models.py
@@ -1,43 +1,31 @@
 # from __future__ import unicode_literals
 from django.db import models
-
 from datetime import date, datetime
-
 import re
 
 class UserManager(models.Manager):
     def basic_validator(self, post_data):    
         errors = {}
-
         name_regex = re.compile(r'^[a-zA-Z\s]+$')
         email_regex = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
-        if not name_regex.match(post_data['first_name']):    # test whether a field matches the pattern            
+        if not name_regex.match(post_data['first_name']):        
             errors['first_name'] = "First name must be letters."
-
         if len(post_data['first_name']) < 2:
             errors['first_name'] = 'First name not long enough'
 
-        if not name_regex.match(post_data['last_name']):    # test whether a field matches the pattern            
+        if not name_regex.match(post_data['last_name']):        
             errors['last_name'] = "Last name must be letters."
         if len(post_data['last_name']) < 2:
             errors['last_name'] = 'Last name not long enough'
 
-        if not email_regex.match(post_data['email']):    # test whether a field matches the pattern            
+        if not email_regex.match(post_data['email']):          
             errors['email'] = "Invalid email address!"
-
-        # try:
-        #     user = User.objects.get(email = post_data['email'])
-        #     errors['email'] = 'Email is already in use.'
-        # except:
-        #     pass
-
         if len(User.objects.filter(email = post_data['email'])) != 0:
             errors['email'] = 'Email is already in use.'
 
         if len(post_data['password']) < 8 :               
             errors['password_length'] = "Invalid password!"
-
         if (post_data['password']) != (post_data['confirm_password']) :               
             errors['password_match'] = "Did not confirm password!"
 
@@ -48,23 +36,24 @@ class User(models.Model):
     first_name = models.CharField(max_length = 255)
     last_name = models.CharField(max_length = 255)
     email = models.CharField(max_length = 320)
+    shoe_size = models.FloatField(null=True)
     password = models.CharField(max_length = 60)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     objects = UserManager()
 
 class Shipping_Address(models.Model):
-    shipping_address = models.CharField(max_length = 255)
-    shipping_city = models.CharField(max_length = 255)
-    shipping_zip = models.IntegerField()
-    user = models.ForeignKey(User, related_name="shipping_address", on_delete = models.CASCADE)
+    shipping_address = models.CharField(max_length = 255, null=True)
+    shipping_city = models.CharField(max_length = 255, null=True)
+    shipping_zip = models.IntegerField(null=True)
+    user = models.ForeignKey(User, related_name="shipping_info", on_delete = models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 class Billing_Address(models.Model):
-    billing_address = models.CharField(max_length = 255)
-    billing_city = models.CharField(max_length = 255)
-    billing_zip = models.IntegerField()
+    billing_address = models.CharField(max_length = 255, null=True)
+    billing_city = models.CharField(max_length = 255, null=True)
+    billing_zip = models.IntegerField(null=True)
     user = models.ForeignKey(User, related_name="billing_address", on_delete = models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -104,7 +93,7 @@ class Inventory(models.Model):
     seller = models.ForeignKey(User, related_name="sold", on_delete = models.CASCADE)
 
 class Shoe(models.Model):
-    size = models.FloatField()
+    item_size = models.FloatField()
     item_sex = models.CharField(max_length = 255)
     item_info = models.ForeignKey(Inventory, related_name="item_type", on_delete = models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
--- login_register_app/views.py
+++ login_register_app/views.py
@@ -3,6 +3,10 @@ import bcrypt
 from django.contrib import messages
 from login_register_app.models import User, UserManager, Inventory, Shipping_Address, Billing_Address, Shoe
 
+
+
+
+
 def index(request):
     return redirect('/home')
 
@@ -19,7 +23,7 @@ def register(request):
         if len(errors) != 0:
             for key, value in errors.items():
                 messages.error(request, value)
-            return redirect('/home')
+            return redirect('/login_page')
         else:
 
             # messages.info(request, "You have successfully registered.")   YOU THIS FIRST TO CHECK THAT ALL THE VALIDATIONS WORK
@@ -29,7 +33,7 @@ def register(request):
             
             messages.info(request, "You have successfully created an account. Log in to get started.")
 
-        return redirect("/home")
+        return redirect("/login_page")
 
 def login(request):
 
@@ -38,16 +42,16 @@ def login(request):
             user = User.objects.get(email=request.POST['email']) 
         except:
             messages.error(request, 'Invalid e-mail address or password.')
-            return redirect('/home')
+            return redirect('/login_page')
         if not bcrypt.checkpw(request.POST['password'].encode(), user.password.encode()):
             messages.error(request, 'Invalid e-mail address or password.')
-            return redirect('/home')
+            return redirect('/login_page')
         else:
             request.session['user_id'] = user.id
             request.session['user_email'] = user.email
             request.session['user_first_name'] = user.first_name
             return redirect('/success')
-        return redirect('/home')
+        return redirect('/login_page')
 
 def success(request):
     if not 'user_id' in request.session:
@@ -61,9 +65,6 @@ def log_out(request):
     del request.session['user_email']
     del request.session['user_first_name']
     return redirect("/home")
-
-
-
 
 def item_info(request, inventory_id):
     context = {
@@ -79,6 +80,32 @@ def user_info(request):
     }
     return render(request, "user_info.html", context)
 
+
+def edit_user(request):
+    user = User.objects.get(id=request.session['user_id'])
+    context={
+        "user": user
+    }
+    return render(request, "user_update.html", context)
+
+def update_user(request):
+    user = User.objects.get(id=request.session['user_id'])
+    user.first_name = request.POST['first_name']
+    user.last_name = request.POST['last_name']
+    user.email = request.POST['email']
+    user.shoe_size = request.POST['shoe_size']
+    user.save()
+    Shipping_Address.objects.create(shipping_address = request.POST("shipping_address"), shipping_city = request.POST("shipping_city"), shipping_zip = request.POST("shipping_zip"), user = user)
+
+    # Billing_Address.objects.create(billing_address = request.POST("billing_address"), billing_city = request.POST("billing_city"), billing_zip = request.POST("billing_zip"), user = user)
+
+    return redirect('/user')
+
+
+
+
+
+
 def new_item(request):
     context = {
         "user" : User.objects.get(id = request.session['user_id'])
@@ -88,23 +115,24 @@ def new_item(request):
 def create_item(request):
     user = User.objects.get(id = request.session['user_id'])
     Inventory.objects.create(
-    item_type= request.POST["item"],
-    item_brand = request.POST["brand"], 
-    item_name= request.POST["name"], 
-    item_primary_color = request.POST["color_one"], 
-    item_secondary_color = request.POST["color_two"], 
-    item_price = request.POST['price'], 
-    front_img = request.POST["front_photo"], 
-    back_img = request.POST["back_photo"], 
-    top_img = request.POST["top_photo"], 
-    bottom_img = request.POST["bottom_photo"], 
-    left_img = request.POST["left_photo"], 
-    right_img = request.POST["right_photo"], 
-    condition = request.POST["condition"],
-    availibaility = True, 
-    seller = user,
-    size = request.POST["size"], 
-    sex = request.POST["gender"])
+        item_type= request.POST["item"],
+        item_brand = request.POST["brand"], 
+        item_name= request.POST["name"], 
+        item_primary_color = request.POST["color_one"], 
+        item_secondary_color = request.POST["color_two"], 
+        item_price = request.POST['price'], 
+        front_img = request.POST["front_photo"], 
+        back_img = request.POST["back_photo"], 
+        top_img = request.POST["top_photo"], 
+        bottom_img = request.POST["bottom_photo"], 
+        left_img = request.POST["left_photo"], 
+        right_img = request.POST["right_photo"], 
+        condition = request.POST["condition"],
+        availibaility = True, 
+        seller = user,
+        # item_size = request.POST["size"], 
+        # item_sex = request.POST["gender"]
+    )
     return redirect('/home')
 
 def checkout(request):
